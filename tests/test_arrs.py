fe460e796ed9f0d171d76caa8c2499ab4de7ee8e
alexander.mescheryakov <alexander.mescheryakov@semrush.com>
2023-07-15 13:03:22 +0200

    fix test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,4 +1,7 @@
+import pytest
+
 from utils import arrs
+from utils.arrs import my_slice
 
 
 def test_get():
@@ -6,6 +9,21 @@ def test_get():
     assert arrs.get([], 0, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+@pytest.fixture()
+def data_for_test_slice():
+    return [([], 0, 1, []),
+            ([1, 2, 3, 4, 5, 6, 7], 0, 0, []),
+            ([1, 2, 3, 4, 5, 6, 7], 7, None, []),
+            ([1, 2, 3, 4, 5, 6, 7], -2, None, [6, 7]),
+            ([1, 2, 3, 4, 5, 6, 7], -5, -2, [3, 4, 5]),
+            ([], 0, -1, []),
+            ([], None, None, []),
+            ([-1], -2, None, [-1]),
+    ]
+
+
+
+def test_my_slice(data_for_test_slice):
+    for coll, start, end, expected  in data_for_test_slice:
+        print(coll, start, end, expected)
+        assert my_slice(coll, start, end) == expected
