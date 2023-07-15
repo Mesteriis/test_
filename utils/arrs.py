9d12ab4935dc981877f140cdfd0acd87747cd534
Sourcery AI <>
2023-07-15 10:42:15 +0000

    'Refactored by Sourcery'

--- utils/arrs.py
+++ utils/arrs.py
@@ -12,10 +12,7 @@ def get(array, index, default=None):
     :return: значение по индексу или значение по-умолчанию.
     """
 
-    if index < 0 or len(array) < index + 1:
-        return default
-
-    return array[index]
+    return default if index < 0 or len(array) < index + 1 else array[index]
 
 
 def my_slice(coll, start=0, end=None):
